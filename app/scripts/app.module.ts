03967fe5bdb6abc2268c1dd109d397a605c76458
corwin <corwinmoyne@gmail.com>
2016-03-10 21:44:02 +0000

    injector task added for scripts and css

---
+++ app/scripts/app.module.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../typings/_reference.ts" />
+
+module app {
+    'use strict';
+
+    var requiredModules: any[] = [
+        'ui.router'
+    ];
+    
+    angular.module('app', requiredModules);
+}
--- typings/_reference.ts
+++ typings/_reference.ts
@@ -1,6 +1,6 @@
 /// <reference path="./tsd.d.ts" />
 
 //grunt-start
-/// <reference path="../app/scripts/app.ts" />
+/// <reference path="../app/scripts/app.module.ts" />
 /// <reference path="../app/scripts/main/main.controller.ts" />
 //grunt-end
