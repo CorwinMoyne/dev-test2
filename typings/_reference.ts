1670fdd46a4d71e5c8fc1a6844cd4cb62791ccc7
Corwin Moyne <corwinmoyne@gmail.com>
2016-03-10 21:52:29 +0000

    _reference.ts deleted online with Bitbucket

--- typings/_reference.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="./tsd.d.ts" />
-
-//grunt-start
-/// <reference path="../app/scripts/app.module.ts" />
-/// <reference path="../app/scripts/main/main.controller.ts" />
-//grunt-end
