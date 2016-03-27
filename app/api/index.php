ca4da6f6d758f7253d724e82bdad64d3739bfdad
corwin <corwinmoyne@gmail.com>
2016-03-27 22:24:43 +0100

    database connection added

--- app/api/index.php
+++ app/api/index.php
@@ -1,38 +1,29 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-date_default_timezone_set('UTC');
+require 'vendor/autoload.php';
+$app = new \Slim\Slim();
 
-try {
-    // Initialize Composer autoloader
-    if (!file_exists($autoload = __DIR__ . '/vendor/autoload.php')) {
-        throw new \Exception('Composer dependencies not installed. Run `make install --directory app/api`');
-    }
-    require_once $autoload;
+$app->get('/example', 'getExample');
+$app->run();
 
-    // Initialize Slim Framework
-    if (!class_exists('\\Slim\\Slim')) {
-        throw new \Exception(
-            'Missing Slim from Composer dependencies.'
-            . ' Ensure slim/slim is in composer.json and run `make update --directory app/api`'
-        );
-    }
+function getExample() {
+  $sql = "select * FROM exampleTable";
+  try {
+    $db = getConnection();
+    $stmt = $db->query($sql);  
+    $wines = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    echo json_encode($wines);
+  } catch(PDOException $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+  }
+}
 
-    // Run application
-    $app = new \Api\Application();
-    $app->run();
-
-} catch (\Exception $e) {
-    if (isset($app)) {
-        $app->handleException($e);
-    } else {
-        http_response_code(500);
-        header('Content-Type: application/json');
-        echo json_encode(array(
-            'status' => 500,
-            'statusText' => 'Internal Server Error',
-            'description' => $e->getMessage(),
-        ));
-    }
+function getConnection() {
+  $dbhost="";
+  $dbuser="root";
+  $dbpass="";
+  $dbname="";
+  $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);  
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $dbh;
 }
