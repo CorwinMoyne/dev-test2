09974ec3d04edb2d8e3fa55ef7726b4695922db6
paul daly <paul daly>
2016-04-08 19:41:06 +0100

    about us page

--- app/scripts/app.module.ts
+++ app/scripts/app.module.ts
@@ -12,7 +12,8 @@ module app {
         'app.home',
         'app.headstone',
         'app.headstones',
-        'app.contact'
+        'app.contact',
+        'app.about'
     ];
     angular.module('app', requiredModules);
 }
