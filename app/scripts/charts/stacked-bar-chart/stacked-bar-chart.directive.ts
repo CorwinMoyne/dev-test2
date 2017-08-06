0ce4460c466c07ee8e74b2c1b1c63acd22ce507d
Corwin Moyne <corwinmoyne@gmail.com>
2017-08-07 00:30:50 +0100

    job families updated

--- app/scripts/charts/stacked-bar-chart/stacked-bar-chart.directive.ts
+++ app/scripts/charts/stacked-bar-chart/stacked-bar-chart.directive.ts
@@ -10,74 +10,77 @@ module app.charts {
         };
         link: ng.IDirectiveLinkFn = (scope: ng.IScope, element: any) => {
             let data = scope['data'];
-            console.log(data);
-            let margin = { top: 0, right: 0, bottom: 0, left: 0 },
-                width = 500 - margin.left - margin.right,
-                height = 400 - margin.top - margin.bottom;
             let colours = ['#1d8074', '#e0e76f', '#e0e2e0'];
-            let values = [];
-            data.forEach(d => {
-                values.push(d.needs);
-                values.push(d.sharedNeedsmore);
-                values.push(d.sharedProficient);
-            });
             let types = ['sharedProficient', 'sharedNeedsmore', 'needs'];
-            let layers = d3.layout.stack()(types.map(function (type) {
-                return data.map(function (d, i) {
+            let layers = d3.layout.stack()(types.map((type) => {
+                return data.map((d, i) => {
                     return { x: i, y: d[type] };
                 });
             }));
+            let margin = { top: 40, right: 10, bottom: 20, left: 50 };
+            let yGroupMax = d3.max(layers, (layer) => { return d3.max(layer, (d) => { return d.y; }); })
+            let yStackMax = d3.max(layers, (layer) => { return d3.max(layer, (d) => { return d.y0 + d.y; }); });
+            let labels = data.map((d) => { return d.jobFamily; });
 
-            let yGroupMax = d3.max(layers, function (layer) { return d3.max(layer, function (d) { return d.y; }); })
-            let yStackMax = d3.max(layers, function (layer) { return d3.max(layer, function (d) { return d.y0 + d.y; }); });
+            draw();
+            // d3.select(window).on('resize', draw);
 
-            let y = d3.scale.ordinal()
-                .domain(d3.range(data.length))
-                .rangeRoundBands([2, height], .7);
+            function draw(): void {
+                let width = (screen.width / 4) - margin.left - margin.right;
+                let height = 533 - margin.top - margin.bottom;
 
-            let x = d3.scale.linear()
-                .domain([0, yStackMax])
-                .range([0, width]);
+                let yScale = d3.scale.ordinal()
+                    .domain(d3.range(data.length))
+                    .rangeRoundBands([1, height], .7);
 
-            let labels = data.map(function (d) { return d.jobFamily; });
+                let xScale = d3.scale.linear()
+                    .domain([0, yStackMax])
+                    .range([0, width / 2]);
 
-            let svg = d3.select(element[0])
-                .append('svg')
-                .attr('width', width + margin.left + margin.right)
-                .attr('height', height + margin.top + margin.bottom)
-    
-            let layer = svg.selectAll(".layer")
-                .data(layers)
-                .enter()
-                .append("g")
-                .attr("class", "layer")
-                .style("fill", function (d, i) { return colours[i]; });
+                let svg = d3.select(element[0])
+                    .append('svg')
+                    .attr('width', width + margin.left + margin.right)
+                    .attr('height', height + margin.top + margin.bottom)
 
-            layer.selectAll("rect")
-                .data(function (d) { return d; })
-                .enter()
-                .append("rect")
-                .attr("y", function (d) {
-                    return y(d.x);
-                })
-                .attr("x", function (d) { return x(d.y0); })
-                .attr("height", y.rangeBand())
-                .attr("width", function (d) { return x(d.y); });
+                let layer = svg.selectAll('.layer')
+                    .data(layers)
+                    .enter()
+                    .append('g')
+                    .attr('class', 'layer')
+                    .style('fill', (d, i) => { return colours[i]; });
 
-            layer.selectAll('text')
-                .data(layers[0])
-                .enter()
-                .append('text')
-                .text((d, i) => {
-                    return labels[i];
-                })
-                .attr({
-                    'text-anchor': 'start',
-                    "y": function (d, i) {
-                        return y(d.x) - 10;
-                    },
-                    "x": function (d) { return x(d.y0); }
-                }).style("fill", 'grey');
+                layer.selectAll('rect')
+                    .data((d) => { return d; })
+                    .enter()
+                    .append('rect')
+                    .attr({
+                        'y': (d) => {
+                            return yScale(d.x);
+                        },
+                        'x': (d) => {
+                            return xScale(d.y0);
+                        },
+                        'height': yScale.rangeBand(),
+                        'width': (d) => {
+                            return xScale(d.y);
+                        }
+                    });
+
+                layer.selectAll('text')
+                    .data(layers[0])
+                    .enter()
+                    .append('text')
+                    .text((d, i) => {
+                        return labels[i];
+                    })
+                    .attr({
+                        'text-anchor': 'start',
+                        'y': (d, i) => {
+                            return yScale(d.x) - 10;
+                        },
+                        'x': (d) => { return xScale(d.y0); }
+                    }).style('fill', 'grey');
+            }
         };
         static instance(): ng.IDirectiveFactory {
             const factory: ng.IDirectiveFactory = (): ng.IDirective => {
